a5d0c03523b270789b91d2f663c8e75cb442504b
jsperger <j.sperger@gmail.com>
2018-03-08 17:11:06 -0500

    Added clinical data to the download script

--- 00_Libs_Data_DL.R
+++ 00_Libs_Data_DL.R
@@ -13,11 +13,20 @@ library("RTCGAToolbox")
 ### Download RNA Seq Data
 ### Function automatically will check if the files have been downloaded
 #Download Size is ~130MB
-ov.rna.query <- GDCquery(project = "TCGA-OV",
+rna.query <- GDCquery(project = "TCGA-OV",
                   data.category = "Gene expression",
                   data.type = "Gene expression quantification",
                   platform = "Illumina HiSeq", 
                   file.type  = "normalized_results",
                   experimental.strategy = "RNA-Seq",
                   legacy = TRUE)
-GDCdownload(ov.query, method = "api", files.per.chunk = 10)
+GDCdownload(rna.query, method = "api", files.per.chunk = 10)
+
+### Download Clinical Data
+### Function automatically will check if the files have been downloaded
+#Download Size is ~32MB
+clinical.query <- GDCquery(project = "TCGA-OV", 
+                  data.category = "Clinical")
+GDCdownload(clinical.query)
+clinical <- GDCprepare_clinic(query, clinical.info = "patient")
+clinical <- GDCquery_clinic(project = "TCGA-OV", type = "clinical")
